fd27e0c2042b9d6b81fd49d1b9e1b0487daa545c
Amory Aubert <amory.aubert@gmail.com>
2019-03-29 12:02:07 +0100

    init projet

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Formulaire de contact</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+<h2>Formulaire de contact</h2>
+<?php
+
+?>
+</body>
+</html>
