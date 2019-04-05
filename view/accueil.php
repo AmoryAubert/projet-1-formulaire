7e82b84151d64cdd15353b46d5d6382b1b9ffe3f
Loïc Marais <maraisloic1208@gmail.com>
2019-04-05 15:10:41 +0200

    css

--- view/accueil.php
+++ view/accueil.php
@@ -1,3 +1,5 @@
 <!-- hackers-poulette-logo.png-->
+<div class='acceuil'>
 <img class='logo' src='https://github.com/becodeorg/CRL-Woods-1.9/blob/master/Projects/HackersPoulette/hackers-poulette-logo.png?raw=true' alt='logo'>
-<h1>Accueil</h1>
+<h1>Le premier pas vers l'excellence</h1>
+</div>
