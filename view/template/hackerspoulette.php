58c0e8a0ff3325dd4fd5a2076daeb36fe904b2d0
Amory Aubert <amory.aubert@gmail.com>
2019-04-05 15:03:40 +0200

    test txt

--- view/template/hackerspoulette.php
+++ view/template/hackerspoulette.php
@@ -3,9 +3,8 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link type="text/css" rel="stylesheet" href="./assets/css/normalize.css">
     <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="./assets/css/style.css">
@@ -23,5 +22,4 @@
     <?php include './view/template/footer.php';?>
 
 </body>
-
 </html>
