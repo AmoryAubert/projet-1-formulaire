1fa3c9ed0c23afb398c2d0e5c0a28601508a1bd5
Loïc Marais <maraisloic1208@gmail.com>
2019-04-05 12:31:45 +0200

    pause midi

--- view/accueil.php
+++ view/accueil.php
@@ -1,2 +1,3 @@
-<img src='https://github.com/becodeorg/CRL-Woods-1.9/blob/master/Projects/HackersPoulette/hackers-poulette-logo.png?raw=true' alt='logo'>
+<!-- hackers-poulette-logo.png-->
+<img class='logo' src='https://github.com/becodeorg/CRL-Woods-1.9/blob/master/Projects/HackersPoulette/hackers-poulette-logo.png?raw=true' alt='logo'>
 <h1>Accueil</h1>
